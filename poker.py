4901e4233d8be07d3a1c937ad25cbe463905f7a9
AutumnThyme <michael_nickerson1@student.uml.edu>
2022-12-03 04:00:53 -0500

    Made poker game and simulated poker game.

--- poker.py
+++ poker.py
@@ -1,63 +1,129 @@
 import copy
 import random
+from poker_utility import *
 
 # Concept, simulate 1000 poker games with x players, removing your hand from the pot.
 # Calculate % wins given your hand given the current knowledge (cards revealed)
-suite = [1,2,3,4]
-ranks = [1,2,3,4,5,6,7,8,9,10,11,12,13]
-
 global_deck = []
-
-for i in suite:
-    for j in ranks:
-        global_deck.append((i,j))
+for suit in Card.suit_mapping.keys():
+    for rank in Card.rank_mapping.keys():
+        global_deck.append(Card(rank, suit))
 
 class Player:
     def __init__(self) -> None:
-        self.cards = []
+        self.hand = Hand()
+        self.investment = 0
         self.folded = False
 
 class PokerGame:
     # From the current move, simulate the game up until a winner.
-    def __init__(self, n, revealed: list, card_a, card_b) -> None:
+    def __init__(self, n: int) -> None:
         self.deck = copy.deepcopy(global_deck)
         self.n = n
         self.players = {
             i: Player()
             for i in range(n)
         }
-        self.revealed = revealed
-        self.card_a = card_a
-        self.card_b = card_b
+        self.me = Player()
         self.rounds_played = 0
 
-    def play(self):
-        self.deal()
-        rounds_left = max(len(self.revealed) - 5, 0)
+    def play_simulated(self, card_a, card_b, revealed):
+        """
+        Play the game assuming you have been dealt card_a and card_b and that cards in the list
+        revealed have already been shown and that the round is the same as the number of revealed cards.
+        @returns 1 if you win, 0 if you tie, and -1 if you draw.
+        """
+        self.deal_simulated(card_a, card_b, revealed)
+        rounds_left =  max(len(revealed) - 5, 0)
         for i in range(rounds_left):
             card = random.choice(self.deck)
             self.deck.remove(card)
-            self.revealed.append(card)
+            self.me.hand.add_card(card)
+            for player in self.players.keys():
+                self.players[player].hand.add_card(card)
 
-    def deal(self):
-        # Remove known cards
-        for card in self.revealed:
+        return self.check_if_i_win()
+
+
+    def play_real(self):
+        """
+        Play the game as normal with no assumptions, being dealt and revealing random cards.
+        @returns 1 if you win, 0 if you tie, and -1 if you draw.
+        """
+        self.deal_real()
+        rounds_left = 5
+        for i in range(rounds_left):
+            card = random.choice(self.deck)
             self.deck.remove(card)
-        self.deck.remove(self.card_a)
-        self.deck.remove(self.card_b)
+            self.me.hand.add_card(card)
+            for player in self.players.keys():
+                self.players[player].hand.add_card(card)
+
+        return self.check_if_i_win()
+
+    def deal_real(self):
+        """
+        Draw two cards for myself and remove from the deck. Let every other player draw two cards as well.
+        """
+        card1 = random.choice(self.deck)
+        self.me.hand.add_card(card1)
+        self.deck.remove(card1)
+
+        card2 = random.choice(self.deck)
+        self.me.hand.add_card(card2)
+        self.deck.remove(card2)
+
+        # Sample cards
+        self.other_players_draw()
+
+    def deal_simulated(self, card_a, card_b, revealed):
+        """
+        Remove the revealed cards from the deck, add my two cards to my hand, and let the other players draw two cards.
+        """
+        for card in revealed:
+            self.deck.remove(card)
+        self.me.hand.add_card(card_a)
+        self.deck.remove(card_a)
+        self.me.hand.add_card(card_b)
+        self.deck.remove(card_b)
         
         # Sample cards
         self.other_players_draw()
 
     def other_players_draw(self):
+        """
+        For each player, draw two cards and remove them from the deck and add them to the players hand. (minus myself)
+        """
         for player in self.players:
             card1 = random.choice(self.deck)
-            self.players[player].cards.append(card1)
+            self.players[player].hand.add_card(card1)
             self.deck.remove(card1)
 
             card2 = random.choice(self.deck)
-            self.players[player].cards.append(card1)
+            self.players[player].hand.add_card(card2)
             self.deck.remove(card2)
 
-    def get_max_combo(self, cards):
-        
+    def check_if_i_win(self):
+        """
+        @returns 1 on a win, 0 on a tie, and -1 on a draw.
+        """
+        value, cards = self.me.hand.hand_value()
+        tie = False
+        for player in self.players.keys():
+            other_value, other_cards = self.players[player].hand.hand_value()
+            if other_value > value:
+                return -1
+            elif other_value == value:
+                my_max_card = max(cards, key=lambda x: x.rank_value)
+                other_max_card = max(other_cards, key=lambda x: x.rank_value)
+                if other_max_card.rank_value > my_max_card.rank_value:
+                    return -1
+                elif other_max_card.rank_value == my_max_card.rank_value:
+                    if other_max_card.suit_value > my_max_card.suit_value:
+                        return -1
+                    elif other_max_card.suit_value == my_max_card.suit_value:
+                        tie = True
+        if tie:
+            return 0
+        else:
+            return 1
--- poker_utility.py
+++ poker_utility.py
@@ -1,40 +1,251 @@
-
-
-def has_royal_flush(cards:list):
-    # A,K,Q,J,10 with same suite. Oh this is a regex problem lmao, well kind of.
-    # Get cards with same suite
-    # for each rank in flush, check if it does not exist.
-    # (suite, rank)
-    ranks = {
-        i: [] for i in range(1,5)
+class Card:
+    suit_mapping = {
+        "clubs": 0,
+        "diamonds": 1,
+        "hearts": 2,
+        "spades": 3
     }
-    for card in cards:
-        ranks[card[0]].append(card[1])
 
-    ranks_with_5 = [r for r in ranks if len(ranks[r]) >= 5]
-    if len(ranks_with_5) == 0:
-        return False
-    elif len(ranks_with_5) > 1:
-        raise "What the fuck?"
-    else:
-        cards = ranks_with_5[0]
-        flush = [1, 11, 12, 13, 10]
-        for card in cards:
-            if card in flush:
-                flush.remove(card)
-        if len(flush) == 0:
-            return True
-    return False
+    rank_mapping = {
+        str(i) for i in range(2,11)
+    }
+    rank_mapping["ace"] = 14
+    rank_mapping["jack"] = 11
+    rank_mapping["queen"] = 12
+    rank_mapping["king"] = 13
 
-def cards_in_straight(cards: list):
-    # Returns all cases of 5 increasing cards.
-    # if card includes an ace, append 14 to the cards since both count.
-    for card in cards:
-        if card[1] == 1:
-            cards.append((card[0], 14))
-            break
-    cards.sort()
+    royals = set(["ace", "king", "queen", "jack", "10"])
 
+    def __init__(self, rank, suit) -> None:
+        self._rank = rank
+        self._suit = suit
 
-def has_straight_flush(cards:list):
-    # 5 consecutive cards all with the same suite.
+    def is_sequential(self, other):
+        """
+        Determines if this card is the next sequential card to another card in a straight.
+        @param other (Card) - The previous card potentially in a straight.
+        @returns True if the card is sequential, False otherwise.
+        """
+        # Consider both cases for other == ace here.
+        if other.rank == "ace":
+            return (self.rank_value - other.rank_value) == 1 or (self.rank_value - 1) == 1
+        else:
+            return (self.rank_value - other.rank_value) == 1
+
+    @property
+    def rank_value(self):
+        return Card.rank_mapping[self._rank]
+    
+    @property
+    def rank(self):
+        return self._rank
+    
+    @rank.setter
+    def rank(self, value):
+        self._rank = value
+
+    @property
+    def suite_value(self):
+        return Card.suit_mapping[self._suit]
+    
+    @property
+    def suite(self):
+        return self._suit
+    
+    @suite.setter
+    def suite(self, value):
+        self._suite = value
+
+class Hand:
+    def __init__(self, cards) -> None:
+        self.cards = cards
+    
+    def add_card(self, card):
+        assert type(card) is Card
+        self.cards.append(card)
+        assert len(self.cards) <= 7
+    
+    def add_cards(self, cards):
+        self.cards.extend(cards)
+        assert len(self.cards) <= 7
+
+    def clear_hand(self):
+        self.cards = []
+
+    def hand_value(self):
+        """
+        @returns a tuple containing the order of the hands value and the cards involved. A smaller order means a higher value.
+        """
+        tests = [
+            self.cards_in_royal_flush,
+            self.cards_in_straight_flush,
+            self.cards_in_four_of_a_kind,
+            self.cards_in_full_house,
+            self.cards_in_flush,
+            self.cards_in_straight,
+            self.cards_in_three_of_a_kind,
+            self.cards_in_two_pair,
+            self.cards_in_pair,
+            self.cards_in_high_card
+        ]
+
+        for idx, test in enumerate(tests):
+            cards = test()
+            if len(cards) != 0:
+                return (idx, cards)
+        # hand must have been empty?
+        raise "Error: An empty hands value is undefined." 
+
+    def cards_in_royal_flush(self):
+        """
+        Get the cards in hand involved with a royal flush.
+        @returns a list of Cards involved with a royal flush.
+        """
+        # There is one suit with more than 5 cards
+        cards = [c for c in self.cards_in_flush() if c.rank in Card.royals]
+        if len(cards) != 5:
+            return []
+        return cards
+
+    def cards_in_straight_flush(self):
+        """
+        Gets the cards involved in a straight flush.
+        @returns a list of cards involved in a straight flush.
+        """
+        flush = self.cards_in_flush()
+        hand = self.cards
+        
+        # temp overwrite hand to test for straight on cards in flush.
+        self.cards = flush
+        flush = self.cards_in_straight()
+
+        # revert back.
+        self.cards = hand
+        return flush
+
+    def cards_in_straight(self):
+        """
+        Gets the cards involved with a straight.
+        @returns a list of cards involved with a straight (5 or more sequential cards)
+        """
+        if len(self.cards) != 5:
+            return []
+        start = 0
+        for i in range(1,len(self.cards)):
+            if not self.cards[i].is_sequential(self.cards[i-1]):
+                diff = i - 1 - start
+                if diff >= 5:
+                    return self.cards[start: i-1]
+                else:
+                    start = i
+        if len(self.cards) - start >= 5:
+            return self.cards[start:]
+        else:
+            return []
+
+    def cards_in_flush(self):
+        """
+        Gets the cards involved with a flush.
+        @returns the cards involved with a flush (same suite)
+        """
+        if len(self.cards) != 5:
+            return []
+
+        # Since max hand is 7, only one flush is possible
+        suits = {x: [] for x in Card.suit_mapping.keys()}
+        for card in self.cards:
+            suits[card.suit].append(card)
+        suits = [s for s in suits.keys() if len(suits[s]) >= 5]
+
+        if len(suits) > 1:
+            raise "Error: invalid count of suites."
+        if len(suits) == 0:
+            return []
+        return suits[0]
+
+    def cards_in_four_of_a_kind(self):
+        """
+        Gets the cards involved in a four of a kind.
+        @returns a list of cards involved in a four of a kind.
+        """
+        kinds = self.get_card_pairs()
+        # 7 cards, impossible to get more than 1x 4 of a kind.
+        kinds = [kinds[kind] for kind in kinds.keys() if len(kinds[kind]) == 4]
+        if len(kinds) == 0:
+            return []
+        if len(kinds) > 1:
+            raise "Error: invalid kind count."
+        return kinds[0]
+
+    def cards_in_full_house(self):
+        """
+        Gets the cards involved in a full house.
+        @returns a list of cards involved in a full house.
+        """
+        # 3 of a kind and 2 of a kind in one hand
+        three_kind = self.cards_in_three_of_a_kind()
+        pair = self.cards_in_pair()
+        if len(three_kind) != 0 and len(pair) != 0:
+            return three_kind.extend(pair)
+        else:
+            return []
+
+    def cards_in_three_of_a_kind(self):
+        """
+        Gets the cards involved in a three of a kind. Splits ties based on which 3kind has the higher rank.
+        @returns a list of cards involved in a three of a kind splitting ties with the highest rank.
+        """
+        kinds = self.get_card_pairs()
+        # 7 cards, impossible to get more than 1x 4 of a kind.
+        kinds = [kinds[kind] for kind in kinds.keys() if len(kinds[kind]) == 3]
+        if len(kinds) == 0:
+            return []
+        if len(kinds) > 2:
+            raise "Error: invalid kind count."
+        return max(kinds, key=lambda x: x[0].rank_value)
+
+    def cards_in_two_pair(self):
+        """
+        Gets the cards involved in a two pair. Splits ties based on which two pair has the higher rank.
+        @returns a list of cards involved in a two pair splitting ties with the highest rank.
+        """
+        kinds = self.get_card_pairs()
+        # 7 cards, impossible to get more than 1x 4 of a kind.
+        kinds = [kinds[kind] for kind in kinds.keys() if len(kinds[kind]) == 2]
+        if len(kinds) < 2:
+            return []
+        if len(kinds) > 3:
+            raise "Error: invalid kind count."
+        kinds.sort(key=lambda x: x[0].rank_value)
+        return kinds[-1].extend(kinds[-2])
+
+    def cards_in_pair(self):
+        """
+        Gets the cards involved in a pair. Splits ties based on which pair has the higher rank.
+        @returns a list of cards involved in a pair splitting ties with the highest rank - doesn't happen in practice since logically this is a two pair.
+        """
+        kinds = self.get_card_pairs()
+        # 7 cards, impossible to get more than 1x 4 of a kind.
+        kinds = [kinds[kind] for kind in kinds.keys() if len(kinds[kind]) == 2]
+        if len(kinds) == 0:
+            return []
+        if len(kinds) > 3:
+            raise "Error: invalid kind count."
+        return max(kinds, key=lambda x: x[0].rank_value)
+
+    def cards_in_high_card(self):
+        """
+        @returns the card with the highest rank_value.
+        """
+        return max(self.cards, key=lambda x: x.rank_value)
+
+    def get_card_pairs(self):
+        """
+        Gets the cards in hand for each rank.
+        @returns a dictionary mapping from each rank to a list containing the cards in hand of said rank.
+        """
+        kinds = {rank: [] for rank in Card.rank_mapping.keys()}
+        for card in self.cards:
+            kinds[card.rank].append(card)
+        return kinds
+
